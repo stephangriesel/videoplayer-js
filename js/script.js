19cba223b9278260cb1fdd82fef4d633ef52185e
Stephan <sgriesel@gmail.com>
2020-02-18 06:58:12 +0100

    update progress barlogic

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 // https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5
+// https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Client-side_web_APIs/Video_and_audio_APIs
 
 const video = document.getElementById('video');
 const play = document.getElementById('play');
@@ -27,12 +28,30 @@ function updatePlayIcon() {
 // Update progress & timestamp
 
 function updateProgress() {
-    return true;
+    console.log(video.currentTime);
+    console.log(video.duration);
+    progress.value = (video.currentTime / video.duration) * 100;
+
+    // Get minutes 
+    let mins = Math.floor(video.currentTime / 60);
+    if (mins < 10) {
+        mins = '0' + String(mins);
+    }
+
+    // Get seconds
+    let secs = Math.floor(video.currentTime % 60);
+    if (secs < 10) {
+        secs = '0' + String(secs);
+    }
+
+    timestamp.innerHTML = `${mins}:${secs}`
+
+
 }
 
 // Time progress
 function setVideoProgress() {
-    return true;
+    video.currentTime = (+progress.value * video.duration) / 100;
 }
 
 // Stop Video
